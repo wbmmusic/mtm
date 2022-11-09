ca0cdb4ce14c50e58ddd535531ade895cc4fffe2
wbmmusic <wbmmusic@gmail.com>
2022-11-09 15:19:09 -0600

    0.0.31

--- electron-wait-react.js
+++ electron-wait-react.js
@@ -3,25 +3,50 @@ const port = process.env.PORT ? (process.env.PORT - 100) : 5000;
 
 process.env.ELECTRON_START_URL = `http://localhost:${port}`;
 
-const client = new net.Socket();
+
+
+const startElectron = () => {
+    console.log('starting electron');
+    startedElectron = true;
+    const exec = require('child_process').exec;
+    var elctrn = exec('yarn run electron');
+
+    elctrn.stdout.on('data', function(data) {
+        console.log(data);
+    });
+
+    elctrn.stdout.on('error', function(data) {
+        console.log("ER", data);
+    });
+
+    // elctrn.stderr.on('data', function(data) {
+    //     console.log('ERRRROOOORRRR', data);
+    // });
+}
 
 let startedElectron = false;
-const tryConnection = () => client.connect({ port: port, host: '127.0.0.1' }, () => {
-    client.end();
-    if (!startedElectron) {
-        console.log('starting electron');
-        startedElectron = true;
-        const exec = require('child_process').exec;
-        var elctrn = exec('yarn run electron');
+const tryConnection = () => {
+    return new Promise((resolve, reject) => {
+        const client = new net.Socket();
+        client.connect({ port: port, host: '127.0.0.1' })
+        client.on('connect', () => {
+            client.end();
+            if (!startedElectron) {
+                startElectron()
+                resolve(true)
+            }
+            resolve(true)
+        })
+        client.on('error', () => {
+            client.destroy()
+            resolve(false)
+        })
+    })
 
-        elctrn.stdout.on('data', function(data) {
-            console.log(data);
-        });
-    }
-});
+};
 
-tryConnection();
 
-client.on('error', (error) => {
-    setTimeout(tryConnection, 1000);
-});
+let timer = setInterval(async() => {
+    const running = await tryConnection()
+    if (running) clearInterval(timer)
+}, 1000);
